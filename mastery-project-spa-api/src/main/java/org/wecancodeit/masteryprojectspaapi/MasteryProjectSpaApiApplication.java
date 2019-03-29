10ef4773998c8c46d8396a900b43fd6ed35cec39
Nate <eclypso@aol.com>
2019-03-29 11:26:03 -0400

    first commit

---
+++ mastery-project-spa-api/src/main/java/org/wecancodeit/masteryprojectspaapi/MasteryProjectSpaApiApplication.java
@@ -0,0 +1,13 @@
+package org.wecancodeit.masteryprojectspaapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MasteryProjectSpaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MasteryProjectSpaApiApplication.class, args);
+	}
+
+}
