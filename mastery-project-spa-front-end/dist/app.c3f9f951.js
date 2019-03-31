3500365d2075333098ddc7b94f4f28ffada62bac
Eythan Kramm <kramm.eythan@gmail.com>
2019-03-31 18:48:11 -0400

    style update

--- mastery-project-spa-front-end/dist/app.c3f9f951.js
+++ mastery-project-spa-front-end/dist/app.c3f9f951.js
@@ -184,7 +184,7 @@ exports.default = Models;
 function Models(models) {
   return "\n  <ul class=\"flex-list\">\n    ".concat(models.map(function (model) {
     return "\n        <li class=\"flex-list__item\">\n          <div class=\"flex-item-container\">\n            <h4 id=\"".concat(model.id, "\" class=\"model__name\"\">").concat(model.modelName, "<h4>\n        </div>\n      </li>\n    ");
-  }).join(''), "\n  </ul>\n \n    <section class=\"add-model\">\n        <input type=\"text\" class=\"add-model__name\" placeholder=\"Model Name\">\n        <input type=\"text\" class=\"add-model__year\" placeholder=\"Model Year\">\n        <input type=\"text\" class=\"add-model__price\" placeholder=\"Model Price\">\n        <input type=\"text\" class=\"add-model__img\" placeholder=\"Image URL\">\n        <input type=\"text\" class=\"add-model__make\" placeholder=\"Model Make\">\n        <button class=\"add-model__submit\">Add Model</button>\n    </section>\n  ");
+  }).join(''), "\n  </ul>\n \n    <section class=\"add-model\">\n        <input type=\"text\" class=\"add-model__name add\" placeholder=\"Model Name\">\n        <input type=\"text\" class=\"add-model__year add\" placeholder=\"Model Year\">\n        <input type=\"text\" class=\"add-model__price add\" placeholder=\"Model Price\">\n        <input type=\"text\" class=\"add-model__img add\" placeholder=\"Image URL\">\n        <input type=\"text\" class=\"add-model__make add\" placeholder=\"Model Make\">\n        <button class=\"add-model__submit\">Add Model</button>\n    </section>\n  ");
 }
 },{}],"js/components/Make.js":[function(require,module,exports) {
 "use strict";
@@ -473,7 +473,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "61372" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "58144" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
--- mastery-project-spa-front-end/js/components/Models.js
+++ mastery-project-spa-front-end/js/components/Models.js
@@ -13,11 +13,11 @@ export default function Models(models) {
   </ul>
  
     <section class="add-model">
-        <input type="text" class="add-model__name" placeholder="Model Name">
-        <input type="text" class="add-model__year" placeholder="Model Year">
-        <input type="text" class="add-model__price" placeholder="Model Price">
-        <input type="text" class="add-model__img" placeholder="Image URL">
-        <input type="text" class="add-model__make" placeholder="Model Make">
+        <input type="text" class="add-model__name add" placeholder="Model Name">
+        <input type="text" class="add-model__year add" placeholder="Model Year">
+        <input type="text" class="add-model__price add" placeholder="Model Price">
+        <input type="text" class="add-model__img add" placeholder="Image URL">
+        <input type="text" class="add-model__make add" placeholder="Model Make">
         <button class="add-model__submit">Add Model</button>
     </section>
   `
