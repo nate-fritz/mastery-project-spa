99dcf3d135e682d096c92f831e9589d00a1b1bb8
sinogue <eclypso@aol.com>
2019-03-31 21:09:08 -0400

    completed css

--- mastery-project-spa-front-end/dist/app.c3f9f951.js
+++ mastery-project-spa-front-end/dist/app.c3f9f951.js
@@ -184,7 +184,7 @@ exports.default = Models;
 function Models(models) {
   return "\n  <ul class=\"flex-list flex-list2\">\n    ".concat(models.map(function (model) {
     return "\n        <li class=\"flex-list__item\">\n          <div class=\"flex-item-container\">\n            <h4 id=\"".concat(model.id, "\" class=\"model__name\"\">").concat(model.modelName, "<h4>\n        </div>\n      </li>\n    ");
-  }).join(''), "\n  </ul>\n\n    <section class=\"add-model\">\n        <input type=\"text\" class=\"add-model__name add\" placeholder=\"Model Name\">\n        <input type=\"text\" class=\"add-model__year add\" placeholder=\"Model Year\">\n        <input type=\"text\" class=\"add-model__price add\" placeholder=\"Model Price\">\n        <input type=\"text\" class=\"add-model__img add\" placeholder=\"Image URL\">\n        <input type=\"text\" class=\"add-model__make add\" placeholder=\"Model Make\">\n        <button class=\"add-model__submit\">Add Model</button>\n    </section>\n\n  ");
+  }).join(''), "\n  </ul>\n\n  ");
 }
 },{}],"js/components/Make.js":[function(require,module,exports) {
 "use strict";
@@ -199,9 +199,7 @@ var _Models = _interopRequireDefault(require("./Models"));
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 function Make(make) {
-
-  return "\n    <img class=\"single-make__img\" src=\"".concat(make.makeImg, "\" alt=\"Company Logo\">\n    <h2 class=\"single-make__name\">").concat(make.makeName, "</h2>\n    ").concat((0, _Models.default)(make.models), "\n    \n\n    <section class=\"add-model\">\n        <input type=\"text\" class=\"add-model__name\" placeholder=\"Model Name\">\n        <input type=\"text\" class=\"add-model__year\" placeholder=\"Model Year\">\n        <input type=\"text\" class=\"add-model__price\" placeholder=\"Model Price\">\n        <input type=\"text\" class=\"add-model__img\" placeholder=\"Image URL\">\n        <button class=\"add-model__submit\" id=\"").concat(make.id, "\">Add Model</button>\n    </section>\n    ");
-
+  return "\n    <div class=\"single-make__div2\">\n        <img class=\"single-make__img2\" src=\"".concat(make.makeImg, "\" alt=\"Company Logo\">\n        <h2 class=\"single-make__name\">").concat(make.makeName, "</h2>\n    </div>\n    ").concat((0, _Models.default)(make.models), "\n    \n\n    <section class=\"add-model\">\n        <input type=\"text\" class=\"add-model__name\" placeholder=\"Model Name\">\n        <input type=\"text\" class=\"add-model__year\" placeholder=\"Model Year\">\n        <input type=\"text\" class=\"add-model__price\" placeholder=\"Model Price\">\n        <input type=\"text\" class=\"add-model__img\" placeholder=\"Image URL\">\n        <button class=\"add-model__submit\" id=\"").concat(make.id, "\">Add Model</button>\n    </section>\n\n    ");
 }
 },{"./Models":"js/components/Models.js"}],"js/components/Model.js":[function(require,module,exports) {
 "use strict";
@@ -499,9 +497,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "61855" + '/');
-
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "59950" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
